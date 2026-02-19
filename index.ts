305146021e09bdcfaa1c8543bffccf84bc931b7b
ElHuzain <a.huzain98@gmail.com>
2026-02-19 21:46:17 +0200

    refactor: voice and state handlers moved to class

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import Discord, { BaseGuildVoiceChannel, ChannelType, GuildMember } from "discord.js";
+import Discord from "discord.js";
 import {
   ANNOUNCEMENT_CHANNEL_ID,
   DAILY_MESSAGE_SEND_HOUR,
@@ -6,12 +6,9 @@ import {
   LOG_CHANNEL_ID,
   TOKEN,
 } from "./src/lib/env.js";
-import handleVoiceStateUpdate from "./src/leaderboardController.js";
+import VoiceStateHandler from "./src/model/voiceStateHandler.js";
 import { init as InitializeDatabase } from "./src/repository/abstractStorage.js";
 import { init as InitDailyMessage } from "./src/dailyMessage.js";
-import UserRepository from "./src/repository/user.js";
-import UserModel from "./src/model/user.js";
-import { IGNORED_VOICE_CHANNEL_IDS } from "./src/lib/env.js";
 
 if (
   ANNOUNCEMENT_CHANNEL_ID === "" ||
@@ -34,47 +31,15 @@ const client = new Client({
   ],
 });
 
+const voiceStateHandler = new VoiceStateHandler();
+
 await InitializeDatabase();
 await InitDailyMessage(client);
 
 client.on(Events.ClientReady, async () => {
-  const repository = new UserRepository();
-  const registeredUsers = await repository.getAllUsers({ hasJoinedTimestamp: true });
-
-  const guild = await client.guilds.fetch(GUILD_ID);
-
-  const allChannels = await guild.channels.fetch();
-  const allVoiceChannels = allChannels.filter(channel => channel?.type === ChannelType.GuildVoice);
-  let userIdsInChannels: string[] = [];
-
-  allVoiceChannels.forEach(async (channel: BaseGuildVoiceChannel) => {
-    channel.members.forEach(async (member: GuildMember) => {
-      if(IGNORED_VOICE_CHANNEL_IDS.includes(channel.id)) return;
-
-      const isRegistered = registeredUsers.some(user => user.id === member.id);
-      if (!isRegistered) {
-        const user = new UserModel({
-          id: member.id,
-          lastJoinedAt: Date.now(),
-        });
-        repository.save(user);
-      } else { 
-        userIdsInChannels.push(member.user.id);
-      }
-    });
-  });
-
-  registeredUsers.forEach(user => {
-    const notInChannels = !userIdsInChannels.includes(user.id);
-
-    if(notInChannels) {
-      user.calculate();
-
-      repository.save(user);
-    }
-  });
+  await voiceStateHandler.syncUsers(client);
 });
 
-client.on(Events.VoiceStateUpdate, handleVoiceStateUpdate);
+client.on(Events.VoiceStateUpdate, (oldState, newState) => voiceStateHandler.handleVoiceStateUpdate(oldState, newState));
 
 client.login(TOKEN);
